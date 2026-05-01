bea6ec2309a09e669be5caf5252b0cbf117b0ea1
Maks Poslovskyi <zamoox@gmail.com>
2026-05-01 20:53:38 +0300

    feat(core): implement functional AuthGuard and protect request creation

---
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -0,0 +1,16 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return true
+  } else {
+    // Редірект на логін, якщо не авторизований
+    router.navigate(['/login']);
+    return false; // Блокуємо перехід
+  }
+};
